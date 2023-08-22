12d59d0f1eee18a23faed483f4da9ba564134807
daoshii <burridge.justin@gmail.com>
2023-08-22 12:27:16 -0500

    Testing github

--- src/main/java/io/github/daoshii/ssskyblock/SSSkyblock.java
+++ src/main/java/io/github/daoshii/ssskyblock/SSSkyblock.java
@@ -1,7 +1,6 @@
 package io.github.daoshii.ssskyblock;
 
 import io.github.daoshii.ssskyblock.managers.CommandManager;
-import io.github.daoshii.ssskyblock.managers.SchematicManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SSSkyblock extends JavaPlugin {
